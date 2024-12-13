7ff83caf630b5813a764f44eb042aa8bf112666c
mic43145 <michael.gysel@fokus.fraunhofer.de>
2024-12-13 11:26:14 +0100

    Updated how private key imported

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 private_key = None
 
 logger.info("APP CONFIG: %s", app.config)
-if "FLASK_PRIVATE_KEY" in app.config: 
+if "PRIVATE_KEY" in app.config: 
     logger.info("Loading Private Key from environment")
     priv_key_string = app.config["PRIVATE_KEY"]
 else:
